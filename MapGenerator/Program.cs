06143c79867f223abe0434578f37bd4f835b6312
Neil Sorensen <nsorens@gmail.com>
2020-09-03 20:40:24 -0600

    Initial commit

---
+++ MapGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MapGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
