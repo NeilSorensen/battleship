4bb6a0d16b6dfadfaaeea61d4e1b5221b37b04e6
Neil Sorensen <nsorens@gmail.com>
2020-09-11 20:30:30 -0600

    We can place ships!

--- MapGenerator.Tests/MapTests.cs
+++ MapGenerator.Tests/MapTests.cs
@@ -1,15 +1,83 @@
+using System;
 using NUnit.Framework;
 
 namespace MapGenerator.Tests
 {
     public class ShipPlacementCheckerTests
     {
-        [TestCase(Direction.North, false)]
-        public void When_placing_a_ship_at_0_0_on_an_empty_map(Direction direction, bool shouldBePlaceable)
+        [TestCase(Direction.North, true)]
+        [TestCase(Direction.East, true)]
+        [TestCase(Direction.South, false)]
+        [TestCase(Direction.West, false)]
+        public void When_placing_the_carrier_at_0_0_on_an_empty_map(Direction direction, bool shouldBePlaceable)
         {
             var classUnderTest = new Map(10, 10);
-            var result = classUnderTest.TryPlace(5, (0,0), direction);
+            var result = classUnderTest.TryPlace(Ship.Carrier, (0,0), direction);
             Assert.That(result, Is.EqualTo(shouldBePlaceable));
+        }
+
+        [TestCase(Direction.North, false)]
+        [TestCase(Direction.East, false)]
+        [TestCase(Direction.South, true)]
+        [TestCase(Direction.West, true)]
+        public void When_placing_the_carrier_at_9_9_on_an_empty_map(Direction direction, bool shouldBePlaceable)
+        {
+            var classUnderTest = new Map(10, 10);
+            var result = classUnderTest.TryPlace(Ship.Carrier, (9,9), direction);
+            Assert.That(result, Is.EqualTo(shouldBePlaceable));
+        }
+
+        [TestCase(Direction.North, true)]
+        [TestCase(Direction.East, true)]
+        [TestCase(Direction.South, true)]
+        [TestCase(Direction.West, true)]
+        public void When_placing_the_destroyer_at_1_1_on_an_empty_map(Direction direction, bool shouldBePlaceable)
+        {
+            var classUnderTest = new Map(10, 10);
+            var result = classUnderTest.TryPlace(Ship.Destroyer, (1,1), direction);
+            Assert.That(result, Is.EqualTo(shouldBePlaceable));
+        }
+
+        [TestCase(Direction.North, true)]
+        [TestCase(Direction.East, true)]
+        [TestCase(Direction.South, false)]
+        [TestCase(Direction.West, false)]
+        public void When_placing_the_carrier_at_1_1_on_an_empty_map(Direction direction, bool shouldBePlaceable)
+        {
+            var classUnderTest = new Map(10, 10);
+            var result = classUnderTest.TryPlace(Ship.Carrier, (1,1), direction);
+            Assert.That(result, Is.EqualTo(shouldBePlaceable));
+        }
+        
+        [TestCase(Direction.North, true)]
+        [TestCase(Direction.East, true)]
+        [TestCase(Direction.South, true)]
+        [TestCase(Direction.West, true)]
+        public void When_placing_the_carrier_at_5_5_on_an_empty_map(Direction direction, bool shouldBePlaceable)
+        {
+            var classUnderTest = new Map(10, 10);
+            var result = classUnderTest.TryPlace(Ship.Carrier, (5,5), direction);
+            Assert.That(result, Is.EqualTo(shouldBePlaceable));
+        }
+
+        [TestCase(Direction.North, false)]
+        [TestCase(Direction.East, false)]
+        [TestCase(Direction.South, true)]
+        [TestCase(Direction.West, true)]
+        public void When_placing_the_carrier_at_6_6_on_an_empty_map(Direction direction, bool shouldBePlaceable)
+        {
+            var classUnderTest = new Map(10, 10);
+            var result = classUnderTest.TryPlace(Ship.Carrier, (6,6), direction);
+            Assert.That(result, Is.EqualTo(shouldBePlaceable));
+        }
+
+        [Test]
+        public void When_placing_a_ship_that_would_intersect_another_ship()
+        {
+            var classUnderTest = new Map(10, 10);
+            classUnderTest.TryPlace(Ship.Battleship, (0, 3), Direction.East);
+            var result = classUnderTest.TryPlace(Ship.Carrier, (0, 0), Direction.North);
+            Assert.That(result, Is.False);
         }
     }
 }
--- MapGenerator/Map.cs
+++ MapGenerator/Map.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace MapGenerator 
 {
@@ -7,15 +10,57 @@ namespace MapGenerator
         private int columnCount;
         private int rowCount;
 
+        private readonly Dictionary<Direction, (int x, int y)> directionVectors = new Dictionary<Direction, (int x, int y)>
+        {
+            {Direction.North, (0, 1)},
+            {Direction.East, (1, 0)},
+            {Direction.South, (0, -1)},
+            {Direction.West, (-1, 0)}
+        };
+
+        private char[,] map;
+
         public Map(int columnCount, int rowCount)
         {
             this.columnCount = columnCount;
             this.rowCount = rowCount;
+            map = new char[columnCount, rowCount];
         }
 
-        public bool TryPlace(int shipLength, (int, int) originPoint, Direction direction)
+        public bool TryPlace(Ship shipToAdd, (int x, int y) originPoint, Direction direction)
         {
+            var vector = directionVectors[direction];
+            var allPoints = new List<(int, int)>();
+            for(int i = 0; i < shipToAdd.Length; i ++)
+            {
+                allPoints.Add((originPoint.x + (vector.x * i), originPoint.y + (vector.y * i)));
+            }
+            if(allPoints.All(IsAvailable))
+            {
+                allPoints.ForEach(x => WriteShipToPoint(shipToAdd, x));
+                return true;
+            }
             return false;
+        }
+
+        private void WriteShipToPoint(Ship shipToAdd, (int x, int y) point)
+        {
+            map[point.x, point.y] = shipToAdd.Marker;
+        }
+
+        private bool IsAvailable((int x, int y) point)
+        {
+            return IsValidLocation(point) && map[point.x, point.y] == '\0';
+        }
+
+        private bool IsValidLocation((int x, int y) point)
+        {
+            return (point.x >= 0 && point.x < columnCount) && (point.y >= 0 && point.y < columnCount);
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(map);
         }
     }
 }
