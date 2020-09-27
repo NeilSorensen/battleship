a236c4e5c14e5c42ffd73542bcbdbc3f365cb8f1
Neil Sorensen <nsorens@gmail.com>
2020-09-27 10:10:51 -0600

    Built the test runs

--- MapGenerator/Program.cs
+++ MapGenerator/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace MapGenerator
 {
     class Program
     {
+        const int MapsPerFile = 1000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Incorrect arguments. Usage: dotnet run -- <outputFilename>");
+            }
+            var maps = new List<Map>();
+            var placer = new BattleshipPlacer();
+            for(int i = 0; i < MapsPerFile; i ++)
+            {
+                maps.Add(placer.GenerateMap());
+            }
+            System.IO.File.WriteAllText(args[0], JsonConvert.SerializeObject(maps.Select(x => x.Serialize()), Formatting.Indented));
         }
     }
 }
